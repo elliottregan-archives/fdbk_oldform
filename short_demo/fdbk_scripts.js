299d9d77325fefb41d6e71261afba7301194176a
Elliott Regan <regan.elliott@gmail.com>
2013-02-06 17:10:28 -0500

    Rmoved jQuery

--- short_demo/fdbk_scripts.js
+++ short_demo/fdbk_scripts.js
@@ -1,18 +1,33 @@
-$(document).ready( function() {
   
-  $('#send').click(function() {
-    $('#form').fadeOut();
-    $('#connect').delay(400).fadeIn(200);
-  })
+function $(id) {
+	return document.getElementById(id);
+}
   
-  $('#connected').click(function() {
-    $('#connect').fadeOut();
-    $('#share').delay(400).fadeIn(200);
-  })
+//  $('#send').click(function() {
+//    $('#form').fadeOut();
+//    $('#connect').delay(400).fadeIn(200);
+//  })
 
-  $('#menu').click( function() {
-    $('#foreground').toggleClass('slide_left');
-  })
+  
+  
+//  $('#connected').click(function() {
+//    $('#connect').fadeOut();
+//    $('#share').delay(400).fadeIn(200);
+//  })
+//
+//  $('#menu').click( function() {
+//    $('#foreground').toggleClass('slide_left');
+//  })
   
 //new FastClick(document.body);
-})
+
+function toStepTwo() {
+  $("form").setAttribute("style", "display: none;");
+  $("connect").setAttribute("style", "display: block;")
+}
+
+function toStepThree() {
+  $("connect").setAttribute("style", "display: none;")
+  $("share").setAttribute("style", "display: block;")
+  
+}
