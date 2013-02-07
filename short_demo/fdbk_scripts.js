1d15d79a73b593d9e059e452923b7881e02372aa
Elliott Regan <regan.elliott@gmail.com>
2013-02-07 11:49:15 -0500

    A few more style changes.

--- short_demo/fdbk_scripts.js
+++ short_demo/fdbk_scripts.js
@@ -1,25 +1,6 @@
-  
 function $(id) {
 	return document.getElementById(id);
 }
-  
-//  $('#send').click(function() {
-//    $('#form').fadeOut();
-//    $('#connect').delay(400).fadeIn(200);
-//  })
-
-  
-  
-//  $('#connected').click(function() {
-//    $('#connect').fadeOut();
-//    $('#share').delay(400).fadeIn(200);
-//  })
-//
-//  $('#menu').click( function() {
-//    $('#foreground').toggleClass('slide_left');
-//  })
-  
-//new FastClick(document.body);
 
 function toStepTwo() {
   $("form").setAttribute("style", "display: none;");
@@ -29,5 +10,4 @@ function toStepTwo() {
 function toStepThree() {
   $("connect").setAttribute("style", "display: none;")
   $("share").setAttribute("style", "display: block;")
-  
 }
