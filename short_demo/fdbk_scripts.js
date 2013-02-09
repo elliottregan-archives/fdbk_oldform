59d18a15f0da140e09dc11451e98c7e43bf94196
Elliott Regan <regan.elliott@gmail.com>
2013-02-09 16:10:00 -0500

    A few styling fixes. No more header.

--- short_demo/fdbk_scripts.js
+++ short_demo/fdbk_scripts.js
@@ -4,15 +4,15 @@ window.addEventListener('load', function() {
 
 function $(id) {
 	return document.getElementById(id);
-}
+};
 
 function toStepTwo() {
+  window.scroll(0,0);
   $("form").setAttribute("style", "display: none;");
-  $("connect").setAttribute("style", "display: block;")
-  $("header").setAttribute("style", "display: block;")
-}
+  $("connect").setAttribute("style", "display: block;");
+};
 
 function toStepThree() {
-  $("connect").setAttribute("style", "display: none;")
-  $("share").setAttribute("style", "display: block;")
-}
+  $("connect").setAttribute("style", "display: none;");
+  $("share").setAttribute("style", "display: block;");
+};
